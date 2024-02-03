3497454b5175999f99b29bc6293f8df9dc1ac892
Jacob <44853261+JacobCollette@users.noreply.github.com>
2024-02-02 20:43:37 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,3 @@
 
-def open_file(filepath):
-    with open(filepath, 'r', encoding='utf-8') as infile:
-        return infile.read()
-
-
 if __name__ == '__main__':
-    prompt = 'Write a list of famous American actors:'
-    response = gpt3_completion(prompt)
-    print(response)
+    print(Hello World)
